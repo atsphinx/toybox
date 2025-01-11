ec799cc647a33ea29c7b2c4bb3e26c66a56cb0de
Kazuya Takei <myself@attakei.net>
2025-01-11 22:22:49 +0900

    docs: Update design
    - Change theme
    - Use back-to-top button

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,3 @@
-from atsphinx.mini18n import get_template_dir as get_mini18n_template_dir
 from atsphinx.toybox.pyproject import load
-
-# -- General configuration
-templates_path = ["_templates", get_mini18n_template_dir()]
 
 load()
