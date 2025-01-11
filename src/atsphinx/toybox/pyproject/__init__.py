e0343013749845e353263622aad8f5fc1b78dd27
Kazuya Takei <myself@attakei.net>
2025-01-12 01:35:22 +0900

    fix: Compatible for python>3.11

--- src/atsphinx/toybox/pyproject/__init__.py
+++ src/atsphinx/toybox/pyproject/__init__.py
@@ -4,7 +4,10 @@ import inspect
 from pathlib import Path
 from typing import Optional
 
-import tomli
+try:
+    import tomllib  # type: ignore[import-not-found]
+except ImportError:
+    import tomli as tomllib
 
 
 def find_pyoroject(conf_py: Path) -> Optional[Path]:  # noqa: D103
@@ -24,7 +27,7 @@ def load():
     pyproject_toml = find_pyoroject(conf_py)
     if pyproject_toml is None:
         raise ValueError()
-    pyproject = tomli.loads(pyproject_toml.read_text())
+    pyproject = tomllib.loads(pyproject_toml.read_text())
     conf_base = pyproject["tool"]["sphinx-build"][conf_py.parent.stem]
     caller.frame.f_locals.update(
         {k: v for k, v in conf_base.items() if not k.startswith("_")}
